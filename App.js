e494060f8a1c64616d8666aba662546642b513bb
ziacdev <68229695+ziacdev@users.noreply.github.com>
2023-10-27 12:42:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,81 @@
+import { NavigationContainer } from "@react-navigation/native";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+
+//const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
+//After installing "npx expo install react-native-gesture-handler react-native-reanimated"
+//add pluging setting in babel.config.js
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="black" />
+      {/* <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            drawerActiveBackgroundColor: "#f0e1ff",
+            drawerActiveTintColor: "#3c0a6b",
+            // drawerStyle: { backgroundColor: "#cccccc" },
+          }}
+        >
+          <Drawer.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              drawerLabel: "Welcome Screen",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              drawerLabel: "User Screen",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer> */}
+
+      <NavigationContainer>
+        <BottomTab.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            tabBarActiveTintColor: "#3c0a6b",
+          }}
+        >
+          <BottomTab.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <BottomTab.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </BottomTab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
